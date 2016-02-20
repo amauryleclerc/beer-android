1ed8e9cf9a3a59b1029de2fb9bdc96527093aa62
Amaury <amauryleclerc@hotmail.fr>
2016-02-20 09:56:59 +0100

    Initial commit

---
+++ app/src/main/java/fr/aleclerc/beer/CategorieListActivity.java
@@ -0,0 +1,13 @@
+package fr.aleclerc.beer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CategorieListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_categorie_list);
+    }
+}
